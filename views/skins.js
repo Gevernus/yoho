3ab3096897fd4b6161e233985f3dc6c6416f2cd1
Artem Povaliaev <gevernus@mail.ru>
2024-09-10 14:21:30 +0400

    Implemented back button logic

--- views/skins.js
+++ views/skins.js
@@ -1,4 +1,9 @@
 export function init(entity) {
+    const inputComponent = entity.getComponent('InputComponent');
+    const backButton = document.getElementById("back-button");
+    backButton.addEventListener("click", () => {
+        inputComponent.addInput("setView", { view: "shop" });
+    });
 };
 
 export function render(entity) {
