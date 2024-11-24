dc82bf6455e5a7e229c50cec47857f98529304a6
Artem Povaliaev <gevernus@mail.ru>
2024-11-24 17:04:57 +0400

    Fixed ton connection method

--- views/bot.js
+++ views/bot.js
@@ -19,7 +19,7 @@ export function init(entity) {
         const wallet = walletComponent.getWallet();
         console.log(`Wallet object`);
         console.log(wallet);
-        if (!wallet.connected) {
+        if (!await wallet.connectionRestored()) {
             alert("Please connect your wallet first");
             return;
         }
